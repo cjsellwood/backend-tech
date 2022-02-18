1312e0a1dcc679993f0b6b0ed072a03d611ff757
cjsellwood <c.sellwood22@gmail.com>
2022-02-18 15:25:09 +1100

    Fix sql github actions error

--- sql/db.ts
+++ sql/db.ts
@@ -2,12 +2,19 @@ import { Pool } from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+let pool: Pool;
+if (process.env.NODE_ENV !== "production") {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+}
 
 export const query = (text: string, params: unknown[]) =>
   pool.query(text, params);
